46cab64eae416f75c32c5cc4b17b7d8af8e7b8b3
Nathan Chau <nateychau@gmail.com>
2020-10-24 20:18:27 -0700

    set up production keys for deployment

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -34,6 +34,7 @@ export const signup = user => dispatch => (
 
 export const login = user => dispatch => (
     APIUtil.login(user).then(res => {
+        console.log(res)
         const { token } = res.data;
         localStorage.setItem('jwtToken', token);
         APIUtil.setAuthToken(token);
--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -54,7 +54,8 @@ class SignupForm extends React.Component {
         password2: this.state.password2
       };
   
-      this.props.signup(user, this.props.history); 
+      this.props.signup(user); 
+      //, this.props.history
     }
   
     renderErrors() {
